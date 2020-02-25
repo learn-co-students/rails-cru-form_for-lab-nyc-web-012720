c7af795ebc6a018625f6e45c115b63efeafebc6e
Jorge Basilio <J.l.Basilio55@gmail.com>
2020-02-25 17:25:41 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+20.times do
+    Artist.create(name: Faker::Music.band, bio: Faker::Quote.yoda)
+end
+
+20.times do
+    Genre.create(name: Faker::Music.genre)
+end
+
+20.times do 
+    Song.create(artist_id: Artist.all.sample.id, genre_id: Genre.all.sample.id, name: Faker::Music::Phish.song)
+end 
+
+
+
